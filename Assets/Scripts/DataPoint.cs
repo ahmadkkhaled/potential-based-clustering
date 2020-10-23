8b9d8507527c69c8e3ba677b38252045bf6f8cb9
unknown <kh-ahmad@protonmail.com>
2020-10-23 18:32:25 +0100

    Added DataPoint class that stores datapoint attributes.

---
+++ Assets/Scripts/DataPoint.cs
@@ -0,0 +1,23 @@
+﻿public class DataPoint
+{
+    public double x, y;
+    public double stretch_x, stretch_y;
+    public double rotation;
+    public double mass;
+
+    public DataPoint() { }
+
+    public DataPoint(double x, double y, double stretch_x, double stretch_y, double rotation, double mass)
+    {
+        this.x = x;
+        this.y = y;
+
+        this.stretch_x = stretch_x;
+        this.stretch_y = stretch_y;
+
+        this.rotation = rotation;
+        this.mass = mass;
+    }
+
+
+}
